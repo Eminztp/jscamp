e5bab30f0047b9e9f674aa5daa7965d466a98181
Eminztp <eminztp@gmail.com>
2021-10-20 20:58:19 +0300

    18 ekim dersi

---
+++ src/app.js
@@ -0,0 +1,7 @@
+let konutKredileri = ["Konut kredisi","Emlak Konut Kredisi","Kamu Konut Kredisi","x kredisi"];
+
+console.log("<ul>")
+for(let i = 0;i<konutKredileri.length;i++){
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
